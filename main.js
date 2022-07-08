cccef2b0274a47aa32c511ee132d8d221668eb82
Emilis Veliullajev <prefis@pm.me>
2022-07-08 21:22:59 +0300

    Enable reset button

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 let board = document.querySelector(".board");
 
 function boardSize(size) {
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-    let pixels = size*size;
+	let pixels = size * size;
 
-    for (let i = 0; i < pixels; i++) {
-        let sq = document.createElement("div");
-        sq.setAttribute("class", "box");
-        board.insertAdjacentElement("beforeend", sq);
-    }
+	for (let i = 0; i < pixels; i++) {
+		let sq = document.createElement("div");
+		sq.setAttribute("class", "box");
+		board.insertAdjacentElement("beforeend", sq);
+	}
+
+    // Reset button
+    document.querySelector("#btnReset").addEventListener("click", function(e) { 
+        let box = document.querySelectorAll(".box");
+        for (let i = 0; i < pixels; i++) {
+            box[i].style.backgroundColor = "white";
+        }    
+    })
 }
 
 function draw() {
-    boardSize(32);
+	boardSize(32);
 
-    const boxes = document.querySelectorAll(".box");
+	const boxes = document.querySelectorAll(".box");
 
-    boxes.forEach(box => {
-        box.addEventListener("mouseover", (e) => {
-            box.style.backgroundColor = "pink";
-        });
-    });
+	boxes.forEach((box) => {
+		box.addEventListener("mouseover", (e) => {
+			box.style.backgroundColor = "pink";
+		});
+	});
 }
 
 draw();
